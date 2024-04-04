01212fc9972d2038d4ce29c072853a0087de92ae
Matthew E Frias <matthew.frias@uconn.edu>
2024-04-04 15:57:04 -0400

    code comments

--- simulation.py
+++ simulation.py
@@ -5,20 +5,19 @@ from string import Template
 
 def apply(args):
     filename = args.name
-    if args.num_resources != None:
+    if args.num != None: # Check if the number of resources is specified
         num_resources = args.num_resources
     else:
         num_resources = 1
 
-    with open(filename) as f:
+    with open(filename) as f: # Read the resource file
         template = Template(f.read())
+        
     try:
         for i in range(1, num_resources + 1):
             cmd_args = ["kubectl", "apply"]
             cmd_args.extend(["-f", "-"])
-            subprocess.run(
-                cmd_args, input=template.substitute(id=i).encode(), check=True
-            )
+            subprocess.run(cmd_args, input=template.substitute(id=i).encode(), check=True) # Apply the resource
     except subprocess.CalledProcessError:
         print(f"Failed to apply resource from file '{filename}'.")
 
@@ -28,23 +27,23 @@ def cluster(args):
     cluster_name = args.name
     if cluster_action == "create":
         try:
-            subprocess.run(["kwokctl", "create", "cluster", "--name", cluster_name], check=True)
+            subprocess.run(["kwokctl", "create", "cluster", "--name", cluster_name], check=True) # Create a cluster
             print(f"Cluster '{cluster_name}' created successfully.")
         except subprocess.CalledProcessError:
             print(f"Failed to create cluster '{cluster_name}'.")
     elif cluster_action == "delete":
         try:
-            subprocess.run(["kwokctl", "delete", "cluster", "--name", cluster_name], check=True)
+            subprocess.run(["kwokctl", "delete", "cluster", "--name", cluster_name], check=True) # Delete a cluster
             print(f"Cluster '{cluster_name}' deleted successfully.")
         except subprocess.CalledProcessError:
-            print(f"Failed to delete cluster '{cluster_name}'.")
+            print(f"Failed to delete cluster '{cluster_name}'.") 
 
 
 def delete(args):
     resource_type = args.type
     resource_name = args.name
     try:
-        subprocess.run(["kubectl", "delete", resource_type, resource_name], check=True)
+        subprocess.run(["kubectl", "delete", resource_type, resource_name], check=True) # Delete the resource
         print(f"Deleted {resource_type} '{resource_name}' successfully.")
     except subprocess.CalledProcessError:
         print(f"Failed to delete {resource_type} '{resource_name}'.")
@@ -53,7 +52,7 @@ def delete(args):
 def get(args):
     resource_type = args.type
     try:
-        subprocess.run(["kubectl", "get", resource_type], check=True)
+        subprocess.run(["kubectl", "get", resource_type], check=True) # Get the resource
     except subprocess.CalledProcessError:
         print(f"Failed to execute 'kubectl get {resource_type}'.")
 
@@ -63,7 +62,7 @@ def show(args):
     try:
         if resource == "nodes":
             cmd = "kubectl describe nodes | awk '/Allocated resources:/,/ephemeral-storage/ {print}'"
-            subprocess.run(cmd, shell=True)
+            subprocess.run(cmd, shell=True) # Show node allocated resources
         elif resource == "priority":
             subprocess.run(
                 [
@@ -75,7 +74,7 @@ def show(args):
                     "-o",
                     "custom-columns=NAME:.metadata.name,PRIORITY:.spec.priorityClassName",
                 ]
-            )
+            ) # See what priority classes are assigned to pods
         elif resource == "pods":
             subprocess.run(
                 [
@@ -84,7 +83,7 @@ def show(args):
                     "pods",
                     "--output=custom-columns=NAMESPACE:.metadata.namespace,NAME:.metadata.name,CPU-REQUEST:.spec.containers[].resources.requests.cpu,CPU-LIMIT:.spec.containers[].resources.limits.cpu,MEMORY-REQUEST:.spec.containers[].resources.requests.memory,MEMORY-LIMIT:.spec.containers[].resources.limits.memory,AGE:.metadata.creationTimestamp",
                 ]
-            )
+            ) # Show pod requests and limits
     except subprocess.CalledProcessError:
         if resource == "nodes":
             print("Failed to show node allocated resources.")
@@ -95,37 +94,37 @@ def show(args):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Script to setup resources in Kubernetes WithOut Kubelet (KWOK)")
-    subparsers = parser.add_subparsers(title="Available Commands")
+    parser = argparse.ArgumentParser(description="Script to setup resources in Kubernetes WithOut Kubelet (KWOK)") # Initialize the command line tool
+    subparsers = parser.add_subparsers(title="Available Commands") # Initialize subparsers for different commands
 
-    apply_parser = subparsers.add_parser("apply", help="Apply specified resource")
-    apply_parser.add_argument("name", help="Filename")
-    apply_parser.add_argument("--num", type=int, help="Number of resources to deploy (optional)")
-    apply_parser.set_defaults(func=apply)
+    apply_parser = subparsers.add_parser("apply", help="Apply specified resource") # Add the apply command
+    apply_parser.add_argument("name", help="Filename") # Add the filename argument
+    apply_parser.add_argument("--num", type=int, help="Number of resources to deploy (optional)") # Add the optional number of resources argument
+    apply_parser.set_defaults(func=apply) # Set the function to apply
 
-    cluster_parser = subparsers.add_parser("cluster", help="Cluster management (create/delete)")
-    cluster_parser.add_argument("action", choices=["create", "delete"], help="Cluster Action")
-    cluster_parser.add_argument("name", help="Cluster Name")
-    cluster_parser.set_defaults(func=cluster)
+    cluster_parser = subparsers.add_parser("cluster", help="Cluster management (create/delete)") # Add the cluster command
+    cluster_parser.add_argument("action", choices=["create", "delete"], help="Cluster Action") # Add the action argument
+    cluster_parser.add_argument("name", help="Cluster Name") # Add the cluster name argument
+    cluster_parser.set_defaults(func=cluster) # Set the function to cluster
 
-    delete_parser = subparsers.add_parser("delete", help="Delete specified resource")
-    delete_parser.add_argument("type", help="Resource Type")
-    delete_parser.add_argument("name", help="Resource Name")
-    delete_parser.set_defaults(func=delete)
+    delete_parser = subparsers.add_parser("delete", help="Delete specified resource") # Add the delete command
+    delete_parser.add_argument("type", help="Resource Type") # Add the resource type argument
+    delete_parser.add_argument("name", help="Resource Name") # Add the resource name argument
+    delete_parser.set_defaults(func=delete)  # Set the function to delete
 
-    get_parser = subparsers.add_parser("get", help="Get specified resource")
-    get_parser.add_argument("type", help="resource type")
-    get_parser.set_defaults(func=get)
+    get_parser = subparsers.add_parser("get", help="Get specified resource") # Add the get command
+    get_parser.add_argument("type", help="resource type") # Add the resource type argument
+    get_parser.set_defaults(func=get) # Set the function to get
 
-    show_parser = subparsers.add_parser("show", help="Show pod characteristics")
-    show_parser.add_argument("resource", choices=["nodes", "priority", "pods"])
-    show_parser.set_defaults(func=show)
+    show_parser = subparsers.add_parser("show", help="Show pod characteristics") # Add the show command
+    show_parser.add_argument("resource", choices=["nodes", "priority", "pods"]) # Add the resource argument
+    show_parser.set_defaults(func=show) # Set the function to show
 
-    args = parser.parse_args()
-    if hasattr(args, "func"):
-        args.func(args)
+    args = parser.parse_args() # Parse the arguments
+    if hasattr(args, "func"): # Check if the function is defined
+        args.func(args) # Execute the function
     else:
-        parser.print_help()
+        parser.print_help() # Print the help message if the function is not defined
 
 
 if __name__ == "__main__":
